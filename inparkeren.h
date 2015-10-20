46bb517c44239f341b9b07e1cd862b97ad289dec
Ryan Schoop <132072@helenparkhurst.net>
2015-10-20 14:44:32 +0200

    Deze moeten nog worden getest en zijn nog niet af. Dit is het eerste deel waarbij een gat moet worden herkent. De situatie is als volgt. De auto rijdt langs een doos of w/e en dan ziet het een gat doordat de afstandssensor niets meet. Ik bedenk me nu echter dat ik ook nog het gedeelte moet implementeren waarbij hij kijkt of het gat groot genoeg is.

---
+++ inparkeren.h
@@ -0,0 +1,4 @@
+void displayProcessed();
+void displayRaw();
+void findEmptySpot(short maxSpeed, char speed);
+void prioToInparkeren(char automatic); // automatic 'Y' means one scripted move, automatic 'N' will mean dynamically.
