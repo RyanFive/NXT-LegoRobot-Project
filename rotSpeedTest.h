7f633f2523155714313c801cc879376e7d30c4c6
Ryan Schoop <132072@helenparkhurst.net>
2015-10-20 14:39:10 +0200

    omkerenMetSteken.c bevat ook een deel rotSpeedTest. Het is niet netjes, maar mijn issue staat beschreven in de vorige commit.

---
+++ rotSpeedTest.h
@@ -0,0 +1,4 @@
+void readAndDisplayAngle();
+void readAndDisplayAngleWhilstMovingReg(char speed, int maxSpeed);
+void readAndDisplayAngleWhilstMoving(char speed, int maxSpeed);
+void omkerenMetSteken(char initialSlave, short maxSpeed, char speed, char syncedTurnRatio, short target);
