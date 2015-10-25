1f937fe2ceaa91c1323d97c3c69fadf4577a3501
Ryan Schoop <132072@helenparkhurst.net>
2015-10-25 17:03:13 +0100

    procedures geschreven om te kijken naar snelheidsverschillen als gevolg van de motorkrachten en een helling.

---
+++ Versnelling_bepalen.h
@@ -0,0 +1,4 @@
+void enlightenSpeedDifferences(int maxSpeed, char moterPowerDiff);
+void enlightenExteriorSpeedDifferences(char speed, int maxSpeed);
+void continualReading(char speed);
+void batchReading(char speed);
