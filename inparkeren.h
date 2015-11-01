7676b37e7e1aeb80ada1fe66760d993ddbc25e87
Ryan Schoop <132072@helenparkhurst.net>
2015-11-01 09:34:51 +0100

    Made the final adjustments to this program. So that it moves enough into the parking lot. Once it is succesfully tested it can be moved to master.

--- inparkeren.h
+++ inparkeren.h
@@ -1,7 +1,10 @@
 void displayProcessed();
 void displayRaw();
 void findEmptySpot(short maxSpeed, char speed, int searchVal);
-void prioToInparkeren(char automatic); // automatic 'Y' means one scripted move, automatic 'N' will mean dynamically.
+void prioToInparkeren(char automatic, short maxSpeed, char speed, double radiusWheel, double radiusBetweenWheels, double turnTimes, int sensorValFromEdge); // automatic 'Y' means one scripted move, automatic 'N' will mean dynamically.
 void inparkeren(short maxSpeed, char speed, double radius/*or float*/, short minParkWidth);
 double degreeTimes(short distance, double radius /*or float*/);
 bool measureHole(short maxSpeed, char speed, int holeSearchVal, double radius, short minParkWidth);
+void park(short maxSpeed, char speed, double radiusWheel, double radiusBetweenWheels, double turnTimes, int sensorValFromEdge);
+double getDistanceFromCircle(double radius, double turnTimes);
+short getTargetFromDistance(double distance, double radius);
